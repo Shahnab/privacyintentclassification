18ea81f5a2fb5ad3ed5ccade9e773a35879d2d64
SHAHNAB AHMED <31092371+Shahnab@users.noreply.github.com>
2023-01-08 19:04:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ st. set_page_config(layout="wide")
 
 st.sidebar.image("https://cdn-icons-png.flaticon.com/512/4104/4104799.png", width=100)
 st.sidebar.subheader("About Privacy Intent Classifier")
-st.sidebar.markdown("##### Understanding privacy policies is crucial for users as it empowers them to learn about the information that matters to them.")
-st.sidebar.markdown("##### In this work, PolicyIE, an English corpus consisting of 5,250 intent and 11,788 slot annotations spanning 31 privacy policies of websites and mobile applications.")
-st.sidebar.markdown("##### PolicyIE corpus is a challenging real-world benchmark with limited labeled examples reflecting the cost of collecting large-scale annotations from domain experts")
+st.sidebar.markdown("###### Understanding privacy policies is crucial for users as it empowers them to learn about the information that matters to them.")
+st.sidebar.markdown("###### In this work, PolicyIE, an English corpus consisting of 5,250 intent and 11,788 slot annotations spanning 31 privacy policies of websites and mobile applications.")
+st.sidebar.markdown("###### PolicyIE corpus is a challenging real-world benchmark with limited labeled examples reflecting the cost of collecting large-scale annotations from domain experts")
 
 
-st.sidebar.subheader("Types of Intents Classified: ")
-st.sidebar.subheader("1. Data Collection/Usage: What, why and how user information is collected")
-st.sidebar.subheader("2. Data Sharing/Disclosure: What, why and how user information is shared with or collected by third parties")
-st.sidebar.subheader("3. Data Storage/Retention: How long and where user information will be stored")
-st.sidebar.subheader("4. Data Security/Protection: Protection measures for user information")
-st.sidebar.subheader("4. Other: Other privacy practices that do not fall into the above four categories")
+st.subheader("Types of Intents Classified: ")
+st.subheader("1. Data Collection/Usage: What, why and how user information is collected")
+st.subheader("2. Data Sharing/Disclosure: What, why and how user information is shared with or collected by third parties")
+st.subheader("3. Data Storage/Retention: How long and where user information will be stored")
+st.subheader("4. Data Security/Protection: Protection measures for user information")
+st.subheader("4. Other: Other privacy practices that do not fall into the above four categories")
 
 
 st.sidebar.caption("Model is trained on PolicyIE dataset")
 st.sidebar.caption("Read more- [link](https://aclanthology.org/2021.acl-long.340.pdf)")
 st.sidebar.caption("App by </Shahnab>")
 
-st.subheader("Voice Cloning Engine")
+st.subheader("Privacy Intent Classification Engine")
 # embed streamlit docs in a streamlit app
 st.components.v1.iframe("https://shad0ws-privacyintentclassifier.hf.space", width=1100, height=650, scrolling=True)
